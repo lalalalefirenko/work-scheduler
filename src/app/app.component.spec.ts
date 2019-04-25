e658bee57b0c8dfbe80fd3de05be6be1f3aaf4dd
l.alefirenko <alefirenkolev@gmail.com>
2019-04-25 12:51:30 +0300

    Reinit project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-sandbox'`, () => {
+  it(`should have as title 'event-scheduler'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-sandbox');
+    expect(app.title).toEqual('event-scheduler');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-sandbox!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to event-scheduler!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DayModule } from './day/day.module';
-import { MatGridListModule } from '@angular/material';
-import { ResponsiveTableComponent } from './responsive-table/responsive-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ResponsiveTableComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    DayModule,
-    MatGridListModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
